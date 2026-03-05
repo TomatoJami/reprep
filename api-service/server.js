3888c967094aaa245a673491155fb5c37f638696
TomatoJami <eduard.toomemets.06@mail.ru>
2026-03-05 13:14:04 +0200

    adding Auth0

--- api-service/server.js
+++ api-service/server.js
@@ -1,11 +1,23 @@
 const express = require("express");
 const app = express()
+const { auth } = require('express-openid-connect');
 
 const PORT = process.env.PORT || 3000;
 const TEAM_NAME = process.env.TEAM_NAME || "Unknown team";
 
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: process.env.AUTH_SECRET || 'saltedstring',
+  baseURL: 'https://reprep.onrender.com/', // или URL деплоя
+  clientID: 'dePHXxBjOOAijoFoExJ1HeJyrj1BxTCt',
+  issuerBaseURL: 'dev-nmxk1zhnhtp63n3q.us.auth0.com'
+};
+
+app.use(auth(config));
+
 app.get("/", (req, res) => {
-    res.send("Hello, boys!");
+    res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
 });
 
 app.get("/api/info", (req, res) => {
